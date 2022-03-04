cd5b976f9e69614dd42dbbe79c0406ac158c0dd9
awashteam <awashteam@gmail.com>
2022-03-04 12:33:03 -0600

    Initial project Add

---
+++ src/main/java/com/lakeelmo/petclinic/lake/elmo/pet/clinic/LakeElmoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.lakeelmo.petclinic.lake.elmo.pet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeElmoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakeElmoPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakeelmo/petclinic/lake/elmo/pet/clinic/LakeElmoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakeelmo.petclinic.lake.elmo.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeElmoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
